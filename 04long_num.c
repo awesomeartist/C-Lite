cbaae8190d838b56caeaa7f981b08788f94c1b0f
xinmindliang <liangxinmind@outlook.com>
2022-06-27 10:05:21 +0800

    optimize

--- 04long_num.c
+++ 04long_num.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 int main(void)
-    {
-        long long a;
-        long long b = 100000;
-        long long c = 640000;
+{
+    long long a;
+    long long b = 100000;
+    long long c = 640000;
 
-        a=0;
-        printf("a = %d \nthe size of a = %d\n",a, sizeof a);
+    a=0;
+    printf("a = %d \nthe size of a = %d\n",a, sizeof a);
 
-        a = 100000*640000;
-        printf("a = %d \nthe size of a = %d\n",a, sizeof a);
+    a = 100000*640000;
+    printf("a = %d \nthe size of a = %d\n",a, sizeof a);
 
-        a = a*b;
-        printf("a = %d \nthe size of a = %d\n",a, sizeof a);
+    a = a*b;
+    printf("a = %d \nthe size of a = %d\n",a, sizeof a);
 
-        return 0;
-    }
+    return 0;
+}
